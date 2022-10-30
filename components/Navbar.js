2f3cba19d26fb05cd97e04f315eb4ec424a01641
success353 <gmacro372@gmail.com>
2022-10-30 20:17:39 +0100

    First commit

---
+++ components/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <nav className='relative container mx-auto p-6'>
+            <div className="flex-center justify-between">
+                <div className="pt-2">
+                    <h1 className='font-bold'>WESY</h1>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 // pages/_app.js
+import '../styles/global.css'
 import { ChakraProvider } from '@chakra-ui/react'
 
 function MyApp({ Component, pageProps }) {
