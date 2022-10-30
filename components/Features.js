88558c2b272c63a179883c99ed84025cfd68c06b
success353 <gmacro372@gmail.com>
2022-10-30 23:35:30 +0100

    First commit

--- components/Features.js
+++ components/Features.js
@@ -17,18 +17,18 @@ const Features = () => {
                 {/* Numbered List */}
                 <div className='flex flex-col space-y-8 md:w-1/2'>
                     {/* List item 1 */}
-                    <div className="flex flex-col space-y-3 md:space-y-0 md-space-x-6 md:flex-row">
+                    <div className="flex flex-col space-y-3 md:space-y-0 md:space-x-6 md:flex-row">
                         {/* Heading */}
                         <div className="rounded-l-full bg-teal-500 md:bg-transparent">
                             <div className="flex items-center space-x-2">
                                 <div className="px-4 py-2 mr-2 text-white rounded-full md:py-1 bg-teal-500">
                                     01
                                 </div>
-                                <h3 className="text-base font-bold md:mb-4 md:hidden">Track company-wide progress</h3>
+                                <h3 className="text-base font-bold md:mb-4 md:hidden">Honesty</h3>
                             </div>
                         </div>
                         <div>
-                            <h3 className="hidden mb-4 text-lg font-bold md:block">Track company-wide progress</h3>
+                            <h3 className="hidden mb-4 text-lg font-bold md:block">Honesty</h3>
                             <p
                                 className="text-gray-500"
                             >
@@ -38,8 +38,51 @@ const Features = () => {
                                 reason why our deals are fair and transparent.
                             </p>
                         </div>
-
-
+                    </div>
+                    {/* List item 2 */}
+                    <div className="flex flex-col space-y-3 md:space-y-0 md:space-x-6 md:flex-row">
+                        {/* Heading */}
+                        <div className="rounded-l-full bg-teal-500 md:bg-transparent">
+                            <div className="flex items-center space-x-2">
+                                <div className="px-4 py-2 mr-2 text-white rounded-full md:py-1 bg-teal-500">
+                                    02
+                                </div>
+                                <h3 className="text-base font-bold md:mb-4 md:hidden">24/7 Service</h3>
+                            </div>
+                        </div>
+                        <div>
+                            <h3 className="hidden mb-4 text-lg font-bold md:block">24/7 Service</h3>
+                            <p
+                                className="text-gray-500"
+                            >
+                                We are always online, 24/7 ready to attend to<br />
+                                your needs. Our customer service is unique in<br />
+                                the sense that they are polite, attentive and warm.
+                            </p>
+                        </div>
+                    </div>
+                    {/* List Item 3 */}
+                    <div className="flex flex-col space-y-3 md:space-y-0 md:space-x-6 md:flex-row">
+                        {/* Heading */}
+                        <div className="rounded-l-full bg-teal-500 md:bg-transparent">
+                            <div className="flex items-center space-x-2">
+                                <div className="px-4 py-2 mr-2 text-white rounded-full md:py-1 bg-teal-500">
+                                    03
+                                </div>
+                                <h3 className="text-base font-bold md:mb-4 md:hidden">Security</h3>
+                            </div>
+                        </div>
+                        <div>
+                            <h3 className="hidden mb-4 text-lg font-bold md:block">Security</h3>
+                            <p
+                                className="text-gray-500"
+                            >
+                                There is a guaranteed zero chance of transaction<br />
+                                failure. Several testimonials confirm our professionalism<br />
+                                and efficiency. We offer the most flexible and prompt<br />
+                                payment you’ll find.
+                            </p>
+                        </div>
                     </div>
                 </div>
 
